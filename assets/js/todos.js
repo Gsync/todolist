9a39cb94872a3230e1913176e8e3b2977bc66f38
Khuram <knshaikh@gmail.com>
2017-06-09 00:37:57 -0600

    enter new todo element

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,14 +2,23 @@ $(document).ready(function() {
 
 
 //Check off specific todos by clicking
-  $("li").click(function() {
+  $("ul").on('click', 'li', function() {
     $(this).toggleClass("completed");
   });
 //Click on X to delete todo
-  $("span").click(function(event) {
+  $("ul").on('click', 'span', function(event) {
       $(this).parent().fadeOut(500, function() {
         $(this).remove();
       });
       event.stopPropagation();
+  });
+
+//enter the todo
+  $("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+      var todoText = $(this).val();
+      $(this).val("");
+      $("ul").prepend("<li><span>X </span>" + todoText + "</li>");
+    }
   });
 });
